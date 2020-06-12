414ad9f22bd30fd179f3b0df4b9ef79ae2badc6c
Kaloyan Valchev <kaloyan.vylchev@gmail.com>
2020-06-12 19:04:34 +0100

    Complete extracting living/birth cities
    Takes user input and loops through it to find "birth" or "live" words and takes the PROPN following those, extracting the cities that user is living and born in.

--- index.py
+++ index.py
@@ -17,22 +17,29 @@ def hi_post():
     input = request.form['CityBorn']
     checkText(input)
     return render_template('index.html', result = input)
+#     I live in Bristol but my birth was given in London
 
 def checkText(inp):
+    """
+    Takes text from user input in the form and extracts cities based on words 'live/living' and 'born/birth'
+    """
     text = nlp(inp)
-    cities = []
-    for word in inp:
-        if (word ==" born " or " birth "):
-            cityBornIn = inp.split(word)                
-        if (word == " live " or " living "):
-            cityLivingIn = inp.split(word)
-    for token in text:
-        # print(token.text, token.pos_, token.dep_)
-        if (token.pos_ == "PROPN" and token.dep_ == "pobj"):
-                cities.append(token)
-    print(cities)
-    print("Birth Sentence: " + " ".join(cityBornIn))
-    print("Life Sentence: " + " ".join(cityLivingIn))
+    city_born_in, city_living_in, city_birth, city_life = '','','',''
+    for i, word in enumerate(text):
+        if word.text == "born" or word.text == "birth":
+            city_born_in = text[i:]
+            for token in city_born_in:
+                if (token.pos_ == "PROPN" and token.dep_ == "pobj"):
+                        city_birth = token
+                        break
+        if word.text == "live" or word.text == "living":
+            city_living_in = text[i:]
+            for token in city_living_in:
+                if (token.pos_ == "PROPN" and token.dep_ == "pobj"):
+                        city_life = token
+                        break
+    print("User was born in " + city_birth.text)
+    print("User lives in " + city_life.text)
 
 if __name__ == '__main__':
     app.run()
@@ -45,6 +52,5 @@ if __name__ == '__main__':
 # 2) Build a user interface in Python/Flask to accept input from the user and get the response back from the NLP processing you did in Assignment. Refactor if necessary.
 
 # 3) Expand on the capability of your NLP. I want to be able to type a sentence like “I was born in Mansfield and now I live in Norwich” and your code should try and recognise which is the city I was born in and which I now live in.
-# I live in Birmingham and my birth was given in South Lake City
 
 # The actual sentence itself will vary, so try and come up with some ways of figuring out what is what in a sentence with two cities in.
